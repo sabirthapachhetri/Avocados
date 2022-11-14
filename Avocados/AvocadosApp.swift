1c940bdd1242b46c49aea4ab374192c1612d0c80
Sabir <sabirthapa19@gmail.com>
2022-11-14 15:43:48 +0545

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Sabir's MacBook on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
