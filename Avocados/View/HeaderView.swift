d5140d8c2b7eec84a9897cbd1e2302980008d770
Sabir <sabirthapa19@gmail.com>
2022-11-16 15:48:55 +0545

    added header and footer to content view

---
+++ Avocados/View/HeaderView.swift
@@ -0,0 +1,67 @@
+//
+//  HeaderView.swift
+//  Avocados
+//
+//  Created by Sabir's MacBook on 11/16/22.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    
+    @State private var showHeadline: Bool = false
+    
+    var slideInAnimation: Animation {
+        Animation.spring(response: 1.5, dampingFraction: 0.5, blendDuration: 0.5)
+            .speed(1)
+            .delay(0.25)
+    }
+    
+    var body: some View {
+        ZStack {
+            
+            Image("avocado-slice-1")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+            
+            HStack(alignment: .top, spacing: 0) {
+                Rectangle()
+                    .fill(Color("ColorGreenLight"))
+                    .frame(width: 4)
+                
+                VStack(alignment: .leading, spacing: 6) {
+                    Text("Avocado")
+                        .font(.system(.title, design: .serif))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                        .shadow(radius: 3)
+                    
+                    Text("Avocados are a powerhouse ingredient at any level for anyone")
+                        .font(.footnote)
+                        .lineLimit(2)
+                        .multilineTextAlignment(.leading)
+                        .foregroundColor(Color.white)
+                        .shadow(radius: 3)
+                }
+                .padding(.vertical, 0)
+                .padding(.horizontal, 20)
+                .frame(width: 281, height: 105)
+                .background(Color("ColorBlackTransparentLight"))
+            }
+            .frame(width: 281, height: 105, alignment: .center)
+            .offset(x: -66, y: showHeadline ? 75 : 220)
+            .animation(slideInAnimation)
+            .onAppear(perform: {
+                self.showHeadline.toggle()
+            })
+        }
+        .frame(width: 480, height: 320, alignment: .center)
+    }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView()
+            .previewLayout(.sizeThatFits)
+    }
+}
